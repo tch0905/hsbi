0388ac2bb0420ce6c834e2e9a13031b3b819c867
tch0905 <a1182116615@gmail.com>
2024-09-27 15:21:31 +0800

    download_data.py

---
+++ utils/download_data.py
@@ -0,0 +1,42 @@
+import csv
+import os
+import requests
+import re
+
+def download_pdf(url, filename):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(filename, 'wb') as f:
+            f.write(response.content)
+        print(f"PDF downloaded and saved as {filename}")
+    else:
+        print(f"Failed to download PDF from {url}")
+
+def main(input_path, output_dir):
+    with open(input_path, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            for header, url in row.items():
+                pattern = r'(\d*)\.HK'
+                match = re.findall(pattern, url)
+                if match:
+                    ric_path = os.path.join(output_dir, match[0])
+                    os.makedirs(ric_path, exist_ok=True)
+                if header == 'SDI':
+                    continue
+                if header == "Financial report":
+                    urls = url.split("\n")
+                    for url in urls:
+                        filename = os.path.basename(url)
+                        file_path = os.path.join(ric_path, filename)
+                        download_pdf(url, file_path)
+                if url.startswith('http'):  # Check if the value is a URL
+                    filename = os.path.basename(url)
+                    file_path = os.path.join(ric_path, filename)
+                    download_pdf(url, file_path)
+
+
+if __name__ == "__main__":
+    path = '../src/data_explorer/faf_documents.csv'
+    output = '../src'
+    main(path,output)
